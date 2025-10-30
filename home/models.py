dfe92ab1683c15750ba73f971c82f5750e90b87a
Teja <tejoabhiram342@gmail.com>
2025-10-30 12:03:03 +0530

    designed a schema for contact form

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=10)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.name
