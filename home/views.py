fbfff18cde80e01f5f5acbd478d85ef7c4de521a
Teja <tejoabhiram342@gmail.com>
2025-10-30 12:47:05 +0530

    adding logic to the contact form and reflect it in database admin

--- home/views.py
+++ home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
+from django.contrib import messages
 
 
 # Create your views here.
@@ -9,7 +11,23 @@ def about(request):
     return render(request, 'home/about.html')
 
 
+
+
+
 def contact(request):
-    return render(request, 'home/contact.html') 
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        content = request.POST.get('content')
+
+        if name and email and phone and content:
+            contact = Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request, "Your message has been sent successfully!")
+        else:
+            messages.error(request, "Please fill all fields.")
+    return render(request, 'home/contact.html')
+
 
 # Create your views here.
